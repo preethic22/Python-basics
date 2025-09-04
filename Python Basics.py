621b7fde82cfd75501f10ad729c278f01b63f3e8
preethic22 <preethic2106@gmail.com>
2025-09-04 21:05:56 +0530

    Add files via upload

---
+++ Python Basics.py
@@ -0,0 +1,88 @@
+#sum of two numbers
+"""first = int(input("enter the  1st number:"))
+second = int(input("enter the 2nd number:"))
+sum = first +second
+print(sum)"""
+
+# Area of Square
+"""side = float(input("enter a side of square:"))
+print("area =" , side * side)"""
+
+# Input 2 floating point numbers and print their average
+"""a= float(input("enter the 1st number:"))
+b= float(input("enter the  2nd number:"))
+print("avg",(a+b)/2)"""
+
+#Print True if a is greater than or equal to b, if not print false.
+"""a = int(input("enter first:"))
+b = int(input("enter second"))
+print(a>=b)"""
+
+#To find Length of the name
+"""name=input("enter the name:")
+print(len(name))"""
+
+#To find given number is even or odd
+"""num=int(input("enter the number:"))
+if(num%2==0):
+    print("even")
+else:
+    print("odd")"""
+    
+#To find greatest of 3 numbers entered by the user
+"""a=int(input("enter the 1st  number:"))
+b=int(input("enter the 2nd  number:"))
+c=int(input("enter the 3rd  number:"))
+if(a>=b and a>=c):
+    print("the 1st largest number",a)
+elif(b>=c):
+    print("the largest number",b)
+else:
+    print("the largest number",c)"""
+
+#To check a number is a multiple of 7 or not.
+"""num=int(input("enter the  number:"))
+if(num%7==0):
+    print("multiple of seven")
+else:
+    print("not multiple of seven")"""
+
+#To enter 3 movies and store them in a list     
+"""movies=[]
+movies.append(input("enter 1st movie:"))
+movies.append(input("enter 2 nd movie:"))
+movies.append(input("enter 3rd movie:"))
+print(movies)"""
+
+#Store the above values in a list and sort them.
+"""grade=["A","D","C","A","B"]
+grade.sort()
+print(grade)"""
+
+
+# To print hello 5 times
+"""count = 1
+while count <=5:
+    print("hello")
+    count += 1  
+print(count)"""
+
+#Print numbers from 1 to 100
+"""i = 1
+while i<=100:
+    print(i)
+    i+=1"""
+
+#Print number from 100  to 1    
+"""i = 100
+while i>=1:
+    print(i)
+    i-=1"""
+    
+#Print the multiplication table of a number n    
+"""n = int(input("enter the number: "))
+i= 1
+while i<=10:
+    print(n*i)
+    i+=1"""
+    
